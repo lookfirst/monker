0a9192055b23d292b745a171e96f79c625c5b46b
Kleber Stangherlin <khstangherlin@gmail.com>
2019-09-25 16:37:22 -0400

    missing order functions

--- monker.py
+++ monker.py
@@ -4,14 +4,14 @@ from datetime import datetime
 from sys import stdout
 from traceback import format_exc as exc
 
-## Print more output to the terminal
+## print more output to the terminal
 VERBOSE = True
 
-## Market and budget definition
-MRKT          = 'ethtusd' ## market to trade on
-MRKT_BUDGET   = 0.0       ## budget for this market (zero for entire balance)
+## market and budget definition
+MRKT_PAIR   = 'eth', 'tusd' ## market to trade on (asset, market)
+MRKT_BUDGET = 0.0           ## budget for market (zero for entire balance)
 
-## Internal state parameters
+## internal state parameters
 TRDN_CHUNK    = 90.0      ## every trade uses the same investiment
 FEES          = 0.50      ## fees to buy/sell
 DTHR_MINIMAL  = FEES*5    ## minimal dip threshold for any transaction
@@ -19,20 +19,19 @@ DTHR_STEP     = FEES      ## dip threshold step to increment/decrement
 TICK_INTERVAL = '5m'      ## window size for market analysis
 TICK_PER_OPER = 5         ## target ticks per operation (1 op every 25min)
 
-## Binance keys and url
+## binance keys and url
 KEY    = 'ZADfFZTF0Djk5HozzmbbPhK1TWqz9SROYaivOcQPbJPIEscP24Rhc8RzMGx7pvdz'
 SECRET = '5SNpXT5wRqDBgEfvl7b2gTLq1fKnNqDmteFZMwXfrbOBKDLSt4QHA7Vu1UcIejYx'
 URL    = 'https://api.binance.com'
 
-## Mongo DB connection
+## mongo db connection
 db_client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = db_client.monker
 
-def logbuy(id, price, qty, target):
+def logbuy(price, qty, target):
     stdout.write('BUY: ')
     obj = {
         'time'   : datetime.now(),
-        'id'     : id,
         'market' : MARKET,
         'price'  : price,
         'target' : target,
@@ -41,28 +40,41 @@ def logbuy(id, price, qty, target):
     if VERBOSE: print(obj)
     db.buy.insert_one(obj)
 
-def logstate(dthr, bln, expsr):
+def logstate():
     stdout.write('STATE: ')
     obj = {
         'time'   : datetime.now(),
+        'market' : MARKET,
         'dthr'   : dthr,
-        'bln'    : bln,
-        'expsr'  : expsr,
+        'blnc'   : blnc,
+        'exps'   : exps,
     }
     if VERBOSE: print(obj)
     db.state.insert_one(obj)
 
-def logsell(id, price, qty):
+def logsell(buy_id, price, qty):
     stdout.write('SELL: ')
     obj = {
         'time'   : datetime.now(),
-        'id'     : id,
         'market' : MARKET,
+        'buy_id' : buy_id,
         'price'  : price,
         'qty'    : qty,
     }
     if VERBOSE: print(obj)
     db.sell.insert_one(obj)
+
+def logdip(dip, dthr, price):
+    stdout.write('DIP: ')
+    obj = {
+        'time'   : datetime.now(),
+        'market' : MARKET,
+        'dip'    : dip,
+        'dthr'   : dthr,
+        'price'  : price,
+    }
+    if VERBOSE: print(obj)
+    db.dip.insert_one(obj)
 
 def logtext(level, text):
     stdout.write('LOGGING: ')
@@ -96,18 +108,46 @@ def get(s, uri, is_signed, **params):
     params = urllib.parse.urlencode(params)
     return s.get(url + uri, params=params)
 
-def get_price(s):
-    ## TODO
-    return 160
+def get_accm_diff(s):
+    ## get last five ticks to calculate price
+    ## sum the derivatives..
+    # returns tuple dip, price
+    return -3.0, 160
 
-def get_balance(s):
-
+def get_mrkt_blnc(s):
+    ## get balance on target market
+    ## ensure it is larger then current market budget 
     return 100
 
-def calculate_exposure(s):
+def cancel_open_buy_orders(s):
+    ## cancel buy open order
+    pass
 
-def 
+def cancel_open_sell_orders(s):
+    ## cancel sell open order
+    pass
 
+def get_opnd_posns(s):
+    ## read open positions from db
+    ## get asset balance from binance
+    ## ensure the asset balance agrees with open positions in db
+    ## TODO I need to decide what will happen when balances dont match
+    pass
+
+def get_mrkt_exps(s):
+    ## TODO I need to decide what will happen when balances dont match
+    pass
+
+def try_buy(price, qty):
+    ## return boolean, true if bought, false if not
+    pass
+
+def try_sell(price, qty):
+    ## return boolean, true if sold, false if not
+    pass
+
+## dip threshold is managed by the main thread
+dthr = None
 
 def buy_thread(stop_event):
     try:
@@ -118,9 +158,21 @@ def buy_thread(stop_event):
             'User-Agent'   : 'monker',
             'X-MBX-APIKEY' : key,
         })
+        cancel_open_buy_orders()
         while not stop_event.is_set():
-            ## buyer loop
-            time.sleep(1)
+            time.sleep(10)
+            dip, price = get_accm_diff(s)
+            if dip < (-dthr):
+                logdip(dip, dthr, price)
+                exps = get_mrkt_exps(s)
+                blnc = get_mrkt_blnc(s)
+                if (exps + blnc >= MRKT_BUDGET and
+                           blnc >= TRDN_CHUNK):
+                    try_buy(price, TRDN_CHUNK):
+                    target = price + dthr
+                    logbuy(price, TRDN_CHUNK, target):
+                else:
+                    logwarn('not enough resources to buy on market')
     except Exception:
         logerror(exc())
 
@@ -133,9 +185,15 @@ def sell_thread(stop_event):
             'User-Agent'   : 'monker',
             'X-MBX-APIKEY' : key,
         })
+        cancel_open_sell_orders()
         while not stop_event.is_set():
-            ## seller loop
-            time.sleep(1)
+            time.sleep(10)
+            posns = get_opnd_posns(s)
+            quote = get_ask_quote(s)
+            for posn in posns:
+                if quote > posn['target']:
+                    try_sell(quote, posn['qty']):
+                    logsell(posn['_id'], quote, posn['qty'])
     except Exception:
         logerror(exc())
 
@@ -152,12 +210,21 @@ def start_sell_thread()
     return thread, stop_event
 
 if __name__ == '__main__':
+    logingo('monker main started')
     try:
+        dthr = DTHR_MINIMAL
         buy_th, buy_stop_ev   = start_buy_thread()
         sell_th, sell_stop_ev = start_sell_thread()
+        while True:
+            time.sleep(10)
+            ## TODO:
+            ## the main thread should do the throtling of dthr up/down
+            ## depending on the throughput of transactions
+            logstate()
     except KeyboardInterrupt:
         buy_stop_ev.set()
         sell_stop_ev.set()
         buy_th.join()
         sell_th.join()
+    logingo('monker main terminated')
 
